6433e7cb7854eb4182873ce7a7aa91cfc1bc7ac6
Isabele-Silva-2024 <iisabelesilva123@gmail.com>
2026-08-11 16:48:31 -0300

    Add files via upload

---
+++ sostudy/footer.php
@@ -0,0 +1,12 @@
+                </div>
+            </div>
+                <!-- Footer-->
+                <footer class="py-2 bg-dark fixed-bottom">
+                    <div class="container"><p class="m-0 text-center text-white">S.O.Study &copy; <?php echo date("Y"); ?> - Sistema Web desenvolvido por Isabele Da Silva e Isabele Jardins </p></div>
+                </footer>
+                <!-- Bootstrap core JS-->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+                <!-- Core theme JS-->
+                <script src="js/scripts.js"></script>
+        </body>
+    </html>        
---
+++ sostudy/formUsuario.php
@@ -0,0 +1,53 @@
+<?php include "header.php" ?>
+    <!-- Page header with logo and tagline-->
+        <header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Bem-Vindo ao S.O.Study</h1>
+                    <p class="lead mb-0"></p>
+                </div>
+            </div>
+        </header>
+    <div class="d-flex justify-content-center mb-3">
+        <h2>Cadastre-se</h2>
+    </div>
+
+    <div class="d-flex justify-content-center mb-3">
+        <form action="actionUsuario.php" method="POST" class="was-validated" enctype="multipart/form-data">
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome Completo" class="form-control">
+                <label for="nomeUsuario">Nome</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+           
+            <div class="form-floating mt-3 mb-3">
+                <input type="email" name="emailUsuario" id="emailUsuario" placeholder="Email" class="form-control">
+                <label for="emailUsuario">Email</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="password" name="senha" id="senha" placeholder="Senha" class="form-control" minlength="3" maxlength="8">
+                <label for="senha">Senha</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="password" name="confirmarsenha" id="confirmarsenha" placeholder="Confirme a Senha" class="form-control" minlength="3" maxlength="8">
+                <label for="confirmarsenha">Confirme a Senha</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <button type="submit" class="btn btn-outline-dark">Cadastrar</button>
+
+        </form>
+
+    </div>
+
+<?php include "footer.php" ?>
