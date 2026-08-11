1ae25591fa827c38e1f6142b508fb3490aa4ae5b
Isabele-Silva-2024 <iisabelesilva123@gmail.com>
2026-08-11 16:49:20 -0300

    Delete sostudy directory

--- sostudy/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start (); //Da início à sessão
-    session_unset(); //Apaga os registros da sessão
-    session_destroy(); //Destrói a sessão
-
-    header("Location: index.php"); //Redireciona o usuário para o formulário de Login 
-    exit();
-?>
